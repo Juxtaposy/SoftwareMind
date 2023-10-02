2d29b36c138246df519f6700cc0943629c341f53
Mateusz Gawronski <thgawronski@gmail.com>
2023-10-02 14:00:21 +0200

    Distribution algorithm implementation

--- src/main/java/mind/distribution/Distribution.java
+++ src/main/java/mind/distribution/Distribution.java
@@ -3,8 +3,39 @@ package mind.distribution;
 import mind.classes.Person;
 import mind.classes.Team;
 
-import java.util.List;
+import java.util.*;
 
-public interface Distribution {
-    List<Team> distributePeople(List<Person> personList);
+public class Distribution {
+    public static List<Team> distributePeopleByOne(LinkedList<Person> personList, int numberOfTeams) {
+        personList.sort(new PersonRateComparator());
+        Deque<Person> personDeque = new LinkedList<>(personList);
+        List<Team> teamList = createTeams(numberOfTeams);
+
+        boolean order = true;
+        while (!personDeque.isEmpty()){
+            for (int i = 0; i < numberOfTeams; i++){
+                try {
+                    if (order) {
+                        teamList.get(i).addPerson(personDeque.pollFirst());
+                    } else {
+                        teamList.get(i).addPerson(personDeque.pollLast());
+                    }
+                }
+                catch (NullPointerException e){
+                    break;
+                }
+            }
+            order = !order;
+        }
+
+        return teamList;
+    }
+
+    private static List<Team> createTeams(int numberOfTeams){
+        List<Team> teamList = new ArrayList<>();
+        for (int i = 0; i < numberOfTeams; i++){
+            teamList.add(new Team());
+        }
+        return teamList;
+    }
 }
